3f503a61414e925979313bab7e02e6252b0153a6
drmeloy <drmeloy@gmail.com>
2020-04-05 14:18:12 -0700

    champ post route test written

--- __tests__/champs.test.js
+++ __tests__/champs.test.js
@@ -17,4 +17,29 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('creates a custom champ', () => {
+    return request(app)
+      .post('/api/v1/champs')
+      .send({
+        name: 'Megaman',
+        passive: 'Lemons',
+        q_ability: 'Buster Cannon',
+        w_ability: 'Jump n\' Shoot',
+        e_ability: 'Summon Rush',
+        r_ability: 'X Armor'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Megaman',
+          passive: 'Lemons',
+          q_ability: 'Buster Cannon',
+          w_ability: 'Jump n\' Shoot',
+          e_ability: 'Summon Rush',
+          r_ability: 'X Armor',
+          __v: 0
+        });
+      });
+  });
 });
